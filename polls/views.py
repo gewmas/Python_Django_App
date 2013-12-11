0372e41b18ce8f77c3a82859aa447f0f0a8f30ba
myhgew <yuhua.mai@duke.edu>
2013-12-11 09:39:48 -0500

    automated testing

--- polls/views.py
+++ polls/views.py
@@ -3,21 +3,31 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_poll_list'
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
 
-    def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+	def get_queryset(self):
+		# """Return the last five published polls (not including those set to be
+		# published in the future)."""
+		return Poll.objects.filter(
+			pub_date__lte=timezone.now()
+			).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-    model = Poll
-    template_name = 'polls/detail.html'
+	model = Poll
+	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+        # """
+        # Excludes any polls that aren't published yet.
+        # """
+		return Poll.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
